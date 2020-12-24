09924af2a65ba04c2f6f8d0f93b79a1080d6e410
iwate-shimizu <kadai.shimizu.senyou@gmail.com>
2020-12-24 17:59:34 +0900

    Fix space to tab

--- public/avatar.js
+++ public/avatar.js
@@ -1,53 +1,53 @@
 class Avatar{
-  x = 0
-  y = 0
-  frame=30
-  angleFlame = 10
-  dx=0
-  dy=0
-  ff=0
-  dAngle =0
-  constructor(name,id,x, y,size,angle){
-    // setup
-    this.name = name
-    this.id = id
-    this.x = x
-    this.y = y
-    this.size = size
-    this.angle = -angle
-  }
-  draw(){
-    push()
-    if(this.ff != 0){
-      this.x += this.dx
-      this.y += this.dy
-      this.ff--
+	x = 0
+	y = 0
+	frame=30
+	angleFlame = 10
+	dx=0
+	dy=0
+	ff=0
+	dAngle =0
+	constructor(name,id,x, y,size,angle){
+		// setup
+		this.name = name
+		this.id = id
+		this.x = x
+		this.y = y
+		this.size = size
+		this.angle = -angle
+	}
+	draw(){
+		push()
+		if(this.ff != 0){
+			this.x += this.dx
+			this.y += this.dy
+			this.ff--
 
-    }
-    let viewColor = "#B5FF14";
-    let ellipseColor ="#FFFFFF";
-  	let pointy = this.size
-  	let pointx = this.size/2
-    // draw
-    translate(this.x,this.y)
-    rotate(radians(this.angle))
-    //triangle(0,0-pointy,0-pointx,0,0+pointx,0)
-    fill(viewColor);
-    arc(0, 0, 90, 90, radians(-135),radians(-45), PIE);
-    fill(ellipseColor);
-    ellipse(0,0,this.size,this.size)
+		}
+		let viewColor = "#B5FF14";
+		let ellipseColor ="#FFFFFF";
+		let pointy = this.size
+		let pointx = this.size/2
+		// draw
+		translate(this.x,this.y)
+		rotate(radians(this.angle))
+		//triangle(0,0-pointy,0-pointx,0,0+pointx,0)
+		fill(viewColor);
+		arc(0, 0, 90, 90, radians(-135),radians(-45), PIE);
+		fill(ellipseColor);
+		ellipse(0,0,this.size,this.size)
 
-    point(this.x,this.y)
+		point(this.x,this.y)
 
-    pop()
+		pop()
 
-  }
-  setPosition(x,y,angle){
-    this.angle = angle
-    this.dx = (x-this.x) / this.frame
-    this.dy = (y-this.y) / this.frame
-    this.ff = this.frame    
+	}
+	setPosition(x,y,angle){
+		this.angle = angle
+		this.dx = (x-this.x) / this.frame
+		this.dy = (y-this.y) / this.frame
+		this.ff = this.frame    
 
 
-  }
+	}
 }
--- public/volume.js
+++ public/volume.js
@@ -1,4 +1,4 @@
 function controlVolume(node,value){
-    const localVideo = document.getElementById(node);
-    localVideo.volume = value; 
+	const localVideo = document.getElementById(node);
+	localVideo.volume = value; 
 }
